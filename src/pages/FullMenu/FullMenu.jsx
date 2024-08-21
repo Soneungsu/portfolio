1a66c81507b5d88f53edaa464a66113aa908038e
liam <liam.son@dktechin.com>
2024-08-21 16:26:21 +0900

    modal done

--- src/pages/FullMenu/FullMenu.jsx
+++ src/pages/FullMenu/FullMenu.jsx
@@ -1,12 +1,23 @@
 import React, { useState } from "react";
 import menuList from "../../assets/data/fullmenu";
+import Main from "../Main/Main.jsx";
 
 const FullMenu = (props) => {
-  const [modal, setModal] = useState(false);
+  const [modal, setModal] = useState(true);
+  const closeBtn = () => {
+    setModal(false);
+  };
+
+  if (!modal) {
+    return null;
+  }
 
   return (
     <div className="full-container">
-      <button id="closebtn">X</button>
+      <button id="closebtn" onClick={closeBtn}>
+        X
+      </button>
+
       <ol className="full-menu-wrap">
         {menuList.map((item, index) => {
           return (
