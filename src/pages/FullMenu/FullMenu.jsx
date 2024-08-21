3b583b08f844f0ed4cf2d714b27d46aef2794244
liam <liam.son@dktechin.com>
2024-08-21 10:09:37 +0900

    tt

--- src/pages/FullMenu/FullMenu.jsx
+++ src/pages/FullMenu/FullMenu.jsx
@@ -2,11 +2,17 @@ import React, { useState } from "react";
 import menuList from "../../assets/data/fullmenu";
 
 const FullMenu = (props) => {
+  const [closeBtn, setCloseBtn] = useState(false);
   return (
     <div className="full-container">
+      <button id="closebtn">X</button>
       <ol className="full-menu-wrap">
         {menuList.map((item, index) => {
-          return <li key={index}>{item}</li>;
+          return (
+            <li className="items" key={index}>
+              {item}
+            </li>
+          );
         })}
       </ol>
     </div>
