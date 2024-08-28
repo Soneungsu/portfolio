f64fd57e03d1b6f038d27187a98476c6a4eed523
liam <liam.son@dktechin.com>
2024-08-28 09:45:46 +0900

    done

--- src/assets/data/project.jsx
+++ src/assets/data/project.jsx
@@ -1,18 +1,27 @@
 const project = [
   {
     src: "이거",
-    title: "이거",
-    description: "이거",
+    title: "쇼핑몰 프로젝트",
+    description: "",
+    site: "https://gentle-cucurucho-7cdd09.netlify.app/",
+    href: "https://github.com/Soneungsu/shoppingmall",
+    program: [],
   },
   {
     src: "이거",
-    title: "이거",
-    description: "이거",
+    title: "날씨 프로젝트",
+    description: "",
+    site: "https://sparkly-baklava-95e00b.netlify.app/",
+    href: "https://github.com/Soneungsu/weatherAPI",
+    program: [],
   },
   {
     src: "이거",
-    title: "이거",
-    description: "이거",
+    title: "포트폴리오 사이트",
+    description: "",
+    site: "https://sparkly-baklava-95e00b.netlify.app/",
+    href: "https://github.com/Soneungsu/weatherAPI",
+    program: ["JavaScript", "React", "Sass(SCSS)"],
   },
 ];
 
--- src/pages/Project/Project.jsx
+++ src/pages/Project/Project.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import project from "../../assets/data/project";
+import { Link } from "react-router-dom";
 
 const Project = () => {
   return (
@@ -16,14 +17,18 @@ const Project = () => {
                   <div className="project-imgBox">
                     <img src={item.src} alt="프로젝트로고" />
                   </div>
-                  <div></div>
+                  <div className="project-description">
+                    <h1>{item.title}</h1>
+                    <p>{item.description}</p>
+                  </div>
+                  <div className="project-skill">
+                    {item.program.map((item) => (
+                      <div>{item}</div>
+                    ))}
+                  </div>
                   <div className="project-link">
-                    <a href="" target="_blank">
-                      사이트 바로가기
-                    </a>
-                    <a href="" target="_blank">
-                      코드 보러가기
-                    </a>
+                    <Link to={item.site}>사이트 바로가기</Link>
+                    <Link to={item.href}>코드 보러가기</Link>
                   </div>
                 </li>
               );
